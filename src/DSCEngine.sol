c0a3407fdddda5b904457d60e0eaba33e8578118
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-10 12:11:36 -0500

    update Natspec

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract DSCEngine {}
+///
+/// @title DSCEngine
+/// @author Dustin Stacy
+///
+/// This system is designed to be as minimal as possible
+/// The token maintains a 1 token == $1 peg
+/// This stablecoin has the following properties:
+/// - Exongenous Collateral
+/// - Dollar Pegged
+/// - Algorithmically Stable
+///
+/// Is is similar to DAI if DAI had no governance, no fees
+/// and was only backed by WETH and WBTC
+///
+/// @notice This contract is the core of the DSC System. It
+/// handles all the logic for mining and redeeming DSC, as
+/// well as depositing & withdrawing collateral.
+/// @notice This contract is VERY loosely based on the MakerDAO
+/// DSS (DAI) system.
+///
+contract DSCEngine {
+
+}
--- src/DefiStableCoin.sol
+++ src/DefiStableCoin.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.20;
 import { ERC20Burnable, ERC20 } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
-/**
- * @title DefiStableCoin
- * @author Dustin Stacy
- * @notice Collateral: Exogenous (ETH & BTC)
- * @notice Minting: Algorithmic
- * @notice Relative Stability: Pegged to USD
- *
- * @dev This is the contract meant to be governed by DSCEngine. This contract
- * is just the ERC20 implementation of our stablecoin system.
- */
+///
+/// @title DefiStableCoin
+/// @author Dustin Stacy
+/// Collateral: Exogenous (ETH & BTC)
+/// Minting: Algorithmic
+/// Relative Stability: Pegged to USD
+///
+/// @notice This is the contract meant to be governed by DSCEngine. This contract
+/// is just the ERC20 implementation of our stablecoin system.
+///
 contract DefiStableCoin is ERC20Burnable, Ownable {
     error DefiStableCoin__AmountMustBeMoreThanZero();
     error DefiStableCoin__BurnAmountExceedsBalance();
