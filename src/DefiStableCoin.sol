eb8cf7f7ca4c1c6dc97862f243adbbc34313186d
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-09 19:59:03 -0500

    add stablecoin functions

--- src/DefiStableCoin.sol
+++ src/DefiStableCoin.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import { ERC20Burnable, ERC20 } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+
 /**
  * @title DefiStableCoin
  * @author Dustin Stacy
@@ -11,6 +14,32 @@ pragma solidity ^0.8.20;
  * @dev This is the contract meant to be governed by DSCEngine. This contract
  * is just the ERC20 implementation of our stablecoin system.
  */
-contract DefiStableCoin {
-    constructor() {}
+contract DefiStableCoin is ERC20Burnable, Ownable {
+    error DefiStableCoin__AmountMustBeMoreThanZero();
+    error DefiStableCoin__BurnAmountExceedsBalance();
+    error DefiStableCoin__NotZeroAddress();
+
+    constructor(address _owner) ERC20('Ballast', 'BAL') Ownable(_owner) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DefiStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DefiStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DefiStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DefiStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
