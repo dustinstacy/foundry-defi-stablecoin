0d7b440fa85d5e56555d844594a773e6c02813d7
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-10 13:58:59 -0500

    add functions to get collateral value in USD

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import { ReentrancyGuard } from '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
 import { DefiStableCoin } from './DefiStableCoin.sol';
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { AggregatorV3Interface } from '@chainlink/contracts/interfaces/AggregatorV3Interface.sol';
 
 ///
 /// @title DSCEngine
@@ -31,6 +32,26 @@ import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 ///
 contract DSCEngine is ReentrancyGuard {
     /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    DefiStableCoin private immutable i_dsc;
+
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
+
+    mapping(address token => address priceFeed) private priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private collateralDeposited;
+    mapping(address user => uint256 amountDSCMinted) private dscMinted;
+    address[] private collateralTokens;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+
+    /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
 
@@ -38,21 +59,6 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__ArraysMustBeSameLength();
     error DSCEngine__TokenNotAllowed();
     error DSCEngine__TransferFailed();
-
-    /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
-    //////////////////////////////////////////////////////////////*/
-
-    DefiStableCoin private immutable i_dsc;
-
-    mapping(address token => address priceFeed) private priceFeeds;
-    mapping(address user => mapping(address token => uint256 amount)) private collateralDeposited;
-
-    /*//////////////////////////////////////////////////////////////
-                                 EVENTS
-    //////////////////////////////////////////////////////////////*/
-
-    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
 
     /*//////////////////////////////////////////////////////////////
                                MODIFIERS
@@ -82,6 +88,7 @@ contract DSCEngine is ReentrancyGuard {
         }
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
             priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            collateralTokens.push(tokenAddresses[i]);
         }
         i_dsc = DefiStableCoin(dscAddress);
     }
@@ -95,6 +102,7 @@ contract DSCEngine is ReentrancyGuard {
     ///
     /// @param tokenCollateralAddress the address of the token to deposit as collateral
     /// @param amountCollateral the amount of collateral to deposit
+    ///
     function depositCollateral(
         address tokenCollateralAddress,
         uint256 amountCollateral
@@ -107,15 +115,66 @@ contract DSCEngine is ReentrancyGuard {
         }
     }
 
+    ///
+    /// @param amountDSCToMint The amount of defi stablecoin to mint
+    /// @notice they must have more collateral value than minimum threshold
+    ///
+    function mintDSC(uint256 amountDSCToMint) external moreThanZero(amountDSCToMint) nonReentrant {
+        dscMinted[msg.sender] += amountDSCToMint;
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
+
     function redeemCollateralForDSC() external {}
 
     function redeemCollateral() external {}
-
-    function mintDSC() external {}
 
     function burnDSC() external {}
 
     function liquidate() external {}
 
     function getHealthFactor() external view {}
+
+    /*//////////////////////////////////////////////////////////////
+                            PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUSD) {
+        for (uint256 i = 0; i < collateralTokens.length; i++) {
+            address token = collateralTokens[i];
+            uint256 amount = collateralDeposited[user][token];
+            totalCollateralValueInUSD += getUSDValue(token, amount);
+        }
+        return totalCollateralValueInUSD;
+    }
+
+    function getUSDValue(address token, uint256 amount) public view returns (uint256 usdValue) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeeds[token]);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _revertIfHealthFactorIsBroken(address user) internal view {}
+
+    /*//////////////////////////////////////////////////////////////
+                           PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _getAccountInformation(
+        address user
+    ) private view returns (uint256 totalDSCMinted, uint256 collateralValueInUSD) {
+        totalDSCMinted = dscMinted[user];
+        collateralValueInUSD = getAccountCollateralValue(user);
+    }
+
+    ///
+    /// Returns how close to liquidation the user is
+    /// If a user goes below 1, they can get liquidated
+    ///
+    function _healthFactor(address user) private view returns (uint256) {
+        (uint256 totalDSCMinted, uint256 collateralValueInUSD) = _getAccountInformation(user);
+    }
 }
