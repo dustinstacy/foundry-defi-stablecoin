13a89803b8be0d0a1fbe75bd8b960b94245f71fb
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-09 19:39:50 -0500

    add base contract

---
+++ src/DefiStableCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title DefiStableCoin
+ * @author Dustin Stacy
+ * @notice Collateral: Exogenous (ETH & BTC)
+ * @notice Minting: Algorithmic
+ * @notice Relative Stability: Pegged to USD
+ *
+ * @dev This is the contract meant to be governed by DSCEngine. This contract
+ * is just the ERC20 implementation of our stablecoin system.
+ */
+contract DefiStableCoin {
+    constructor() {}
+}
